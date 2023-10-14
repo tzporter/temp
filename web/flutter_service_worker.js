f2727a514a91e5c9642ce81db176c05cd9ad2e1f
TrumanPorter <72238339+TrumanPorter@users.noreply.github.com>
2023-10-14 14:45:13 -0500

    update 5

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -24,9 +24,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "c4bc0397ebc89a0fc13957786518ba98
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "aa5114567f820b493ec2345e23fceb0c",
-"/": "aa5114567f820b493ec2345e23fceb0c",
-"main.dart.js": "0b9ebf6dc62cce829b65402caec55e9b",
+"index.html": "a373f4d55d9108ed3e75951e7718ffe0",
+"/": "a373f4d55d9108ed3e75951e7718ffe0",
+"main.dart.js": "190364268b1e10486dcc3244ecf4b5f0",
 "manifest.json": "a001bd97706f47180ca638b3dfc84ee5",
 "version.json": "bcc5dce59e64a0ad6c476ac678a14c32"};
 // The application shell files that are downloaded before a service worker can
